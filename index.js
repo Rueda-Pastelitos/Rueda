238ec11cb9f99ebc366f4b694ce01fadf298fb7f
Rueda-Pastelitos <tony150268@gmail.com>
2025-07-28 15:07:06 +0200

    index.js

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.get('/api/testimonianze', (req, res) => {
   const filePath = path.join(__dirname, 'testimonianze.json');
