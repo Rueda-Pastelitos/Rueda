9892515a5a063b988b3e5b0ad95e62d69769ca6e
Rueda-Pastelitos <tony150268@gmail.com>
2025-08-03 10:26:23 +0200

    Update pool.js

--- db/pool.js
+++ db/pool.js
@@ -1,1 +1,9 @@
 //pool;
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+module.exports = pool;
