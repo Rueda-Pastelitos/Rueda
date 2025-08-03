f2bf60177767eb8cf0c455c2efe923258724b194
Rueda-Pastelitos <tony150268@gmail.com>
2025-08-03 10:26:51 +0200

    Update example.js

--- routes/example.js
+++ routes/example.js
@@ -1,1 +1,16 @@
 //routes;
+const express = require('express');
+const router = express.Router();
+const pool = require('../db/pool');
+
+router.get('/', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Errore nel recupero');
+  }
+});
+
+module.exports = router;
