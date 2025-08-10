75995e39ed0d8c509d52e45e9800bbb262cfc9dd
Rueda-Pastelitos <tony150268@gmail.com>
2025-08-10 11:32:28 +0200

    Update pool.js

--- db/pool.js
+++ db/pool.js
@@ -1,9 +1,15 @@
 //pool;
 const { Pool } = require('pg');
 
+const isLocal = process.env.NODE_ENV !== 'production';
+
+const connectionString = isLocal
+  ? 'postgresql://postgres:aSGLozEYytZyNJCXSsEmTyaAAWJxQAol@shuttle.proxy.rlwy.net:32164/railway' // public
+  : 'postgresql://postgres:aSGLozEYytZyNJCXSsEmTyaAAWJxQAol@postgres.railway.internal:5432/railway'; // interna
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  connectionString,
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
