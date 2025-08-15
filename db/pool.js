e553ec6386381302d6fb7da980cad874001267f3
Rueda-Pastelitos <tony150268@gmail.com>
2025-08-15 08:37:46 +0200

    Update pool.js

--- db/pool.js
+++ db/pool.js
@@ -2,7 +2,7 @@
 
 const { Pool } = require('pg');
 
-const connectionString = process.env.DATABASE_URL || 'postgresql://postgres:aSGLozEYytZyNJCXSsEmTyaAAWJxQAol@your-db-host:5432/railway';
+const connectionString = process.env.DATABASE_URL || 'postgresql://postgres:aSGLozEYytZyNJCXSsEmTyaAAWJxQAol@postgres.railway.internal:5432/railway';
 
 const pool = new Pool({
   connectionString,
